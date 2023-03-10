e1912d850bc0557cbe97607f5a39d7e1101f8c90
jhonned01 <jhonned01@gmail.com>
2023-03-10 14:09:27 -0500

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,7 @@ import "./globals.css";
 import Main from "./Main";
 
 export const metadata = {
-  icons: {
-    icon: "/EscudoUniminuto.webp",
-    shortcut: "/EscudoUniminuto.webp",
-    apple: "/EscudoUniminuto.webp",
-    other: {
-      rel: "shortcut icon",
-      url: "/EscudoUniminuto.webp",
-    },
-  },
   title: "SYGESAUNIV",
-
   description:
     "Mejora tu desempeño en el examen Saber Pro con nuestra completa plataforma en línea. Nuestra aplicación web de administración de pruebas te brinda las herramientas necesarias para medir tu progreso, identificar tus fortalezas y áreas de mejora, y aumentar tus posibilidades de éxito en el examen. ¡Regístrate hoy mismo y prepárate para alcanzar tus metas académicas!",
 };
